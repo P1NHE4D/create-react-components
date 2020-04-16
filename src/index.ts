3e3100bfbbdd34983746a58512f328f1b4fe62ca
P1NHE4D <8gerlach@informatik.uni-hamburg.de>
2020-04-16 23:09:54 +0200

    add: project config files

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import fs from 'graceful-fs';
+import { join, parse, relative } from 'path';
+import prompt from 'prompts';
+import exit from 'exit';
+
+export function generateReactComponent(): void {
+    
+}
